d6694da357280a64945af4a3e2e49645d0dfee86
kevin <qiufengkzl@gmail.com>
2024-08-10 16:13:51 +0800

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-sudo -i
-
 # 修改SSH默认端口
-# 你可以将2222替换为你希望的端口号
 NEW_SSH_PORT=10022
 
 # 备份原始的sshd_config文件
 sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 
 # 修改端口
-sudo sed -i "s/#Port 22/Port $NEW_SSH_PORT/" /etc/ssh/sshd_config
+sudo sed -i "s/^Port .*/Port $NEW_SSH_PORT/" /etc/ssh/sshd_config
 
 # 重启SSH服务
 sudo systemctl restart ssh
 
-apt update
+# 更新包列表
+sudo apt update
 
 # 安装Fail2Ban
 sudo apt install -y fail2ban
@@ -23,12 +21,9 @@ sudo apt install -y fail2ban
 # 切换到Fail2Ban配置目录
 cd /etc/fail2ban
 
-# 拷贝配置文件
+# 备份配置文件
 sudo cp fail2ban.conf fail2ban.local
-sudo cp jail.conf jail.local
-
-# 删除现有的jail.local文件中的内容
-sudo echo "" > jail.local
+sudo cp jail.conf jail.local.bak
 
 # 添加[DEFAULT]配置
 sudo bash -c 'cat << EOF >> jail.local
@@ -61,5 +56,7 @@ EOF'
 
 # 重启Fail2Ban服务以应用更改
 sudo systemctl restart fail2ban
-sleep 5
+
+# 检查Fail2Ban状态
+sleep 10
 sudo fail2ban-client status sshd
